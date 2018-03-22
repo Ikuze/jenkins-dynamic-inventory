f93fbe0268b48387925ba2448a5739d2902cce84
Ikuze <37222566+Ikuze@users.noreply.github.com>
2018-03-23 00:08:48 +0100

    Evolve offline tests

--- tests/run-tests.py
+++ tests/run-tests.py
@@ -62,6 +62,22 @@ class JenkinsInventory_Unit_NoCache_Tests(TestCase):
     def get_inventory_file(self):
         return os.path.join(self.test_base_dir, self.test_config[self.INVENTORY_FILE])
 
+    def do_assertions(self, jenkins_inventory):
+        # print (jenkins_inventory.inventory.get_host('node1').vars)
+        # print(jenkins_inventory.inventory.groups )
+        expected_groups = self.test_config['expect']['all_groups']
+        actual_groups = jenkins_inventory.inventory.groups.keys()
+        # remove the _meta group, previously used for cache
+        actual_groups.remove('_meta')
+        self.assertItemsEqual(actual_groups,
+                             expected_groups)
+
+        for group_name, nodes in self.test_config['expect']['groups'].iteritems():
+            one_group = jenkins_inventory.inventory.groups[group_name]
+            group_nodes = [x.name for x in one_group.get_hosts()]
+            self.assertItemsEqual(group_nodes, nodes)
+
+
 
 def make_test_function(description, test_filename):
     @patch('jenkins.build_opener')
@@ -82,21 +98,20 @@ def make_test_function(description, test_filename):
         inventory_file = self.get_inventory_file()
         jenkins_inventory.parse(inventory_mock, loader_mock, inventory_file, cache=False)
 
-        # print (jenkins_inventory.inventory.get_host('node1').vars)
-        for group_name, nodes in self.test_config['expect']['groups'].iteritems():
-            one_group = jenkins_inventory.inventory.groups[group_name]
-            group_nodes = [x.name for x in one_group.get_hosts()]
-            self.assertListEqual(group_nodes, nodes)
+        self.do_assertions(jenkins_inventory)
 
     return test_exec
 
 
 if __name__ == '__main__':
 
-    directory = 'datasets'
-    for test in os.listdir(directory):
+    current_directory = os.path.dirname(os.path.realpath(__file__))
+    tests_directory = 'datasets/tests/'
+    tests_directory = os.path.join(current_directory, tests_directory)
+
+    for test in os.listdir(tests_directory):
         print('Test Added: {0}'.format(test))
-        test_func = make_test_function(test, '{0}/{1}/test.yml'.format(directory, test))
+        test_func = make_test_function(test, '{0}/{1}/test.yml'.format(tests_directory, test))
         setattr(JenkinsInventory_Unit_NoCache_Tests, 'test_{0}'.format(test), test_func)
 
     suite = unittest.TestSuite()
